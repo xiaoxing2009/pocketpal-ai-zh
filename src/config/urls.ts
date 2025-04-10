4f986648cab8f95b2cfb46048d684773304e28b4
a-ghorbani <ghorbani59@gmail.com>
2025-04-10 08:42:02 +0200

    chore: reuse FIREBASE_FUNCTIONS_URL for FEEDBACK_URL

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,4 +1,4 @@
-import {FEEDBACK_URL, FIREBASE_FUNCTIONS_URL} from '@env';
+import {FIREBASE_FUNCTIONS_URL} from '@env';
 
 export const HF_DOMAIN = 'https://huggingface.co';
 export const HF_API_BASE = `${HF_DOMAIN}/api/models`;
@@ -18,5 +18,5 @@ export const urls = {
   benchmarkSubmit: () => `${FIREBASE_FUNCTIONS_URL}/api/v1/submit`,
 
   // Feedback Endpoint
-  feedbackSubmit: () => `${FEEDBACK_URL}/v1/feedback`,
+  feedbackSubmit: () => `${FIREBASE_FUNCTIONS_URL}/feedback`,
 };
